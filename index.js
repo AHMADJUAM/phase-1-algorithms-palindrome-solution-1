f649bb6a60dd053f99277991c5aa1f708f329c87
ahmad juma <juam17020@gmail.com>
2024-02-13 14:58:15 +0300

    solve assignment

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  // Implementation of the isPalindrome function
+  // Check if the word is empty or a single character, it's considered a palindrome
+  if (word.length <= 1) {
+    return true;
+  }
+  
+  // Compare the first and last characters of the word
+  // If they are not equal, the word is not a palindrome
+  if (word[0] !== word[word.length - 1]) {
+    return false;
+  }
+  
+  // Recursively check the substring excluding the first and last characters
+  return isPalindrome(word.slice(1, -1));
 }
 
 module.exports = isPalindrome;
